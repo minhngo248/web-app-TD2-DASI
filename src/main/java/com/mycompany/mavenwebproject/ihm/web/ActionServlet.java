3b27039338fa227412e0f6152948834e726d47a2
minhngo248 <minhngo248@users.noreply.github.com>
2022-03-18 14:32:49 +0100

    update 1.0

--- src/main/java/com/mycompany/mavenwebproject/ihm/web/ActionServlet.java
+++ src/main/java/com/mycompany/mavenwebproject/ihm/web/ActionServlet.java
@@ -5,10 +5,11 @@
  */
 package com.mycompany.mavenwebproject.ihm.web;
 
-import com.mycompany.tdmaven.dao.JpaUtil;
 import com.mycompany.mavenwebproject.ihm.web.action.Action;
 import com.mycompany.mavenwebproject.ihm.web.action.AuthentifierClientAction;
+import com.mycompany.mavenwebproject.ihm.web.serialisation.ProfilClientSerialisation;
 import com.mycompany.mavenwebproject.ihm.web.serialisation.Serialisation;
+import com.mycompany.tdmaven.dao.JpaUtil;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -46,7 +47,7 @@ public class ActionServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
     }
 
     /**
@@ -62,41 +63,41 @@ public class ActionServlet extends HttpServlet {
             throws ServletException, IOException {
         processRequest(request, response);
     }
-    
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-    
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         request.getSession(true); //initialisation de la Session
         request.setCharacterEncoding("UTF-8"); //Encodage de paramètre de la requete (pour
-                                                // une lecture correcte
+        // une lecture correcte
         String todo = request.getParameter("todo");
-        
+
         Action action = null;
         Serialisation serialisation = null;
-        
-        switch(todo) {
+
+        switch (todo) {
             case "connecter": {
                 action = new AuthentifierClientAction();
-                //serialisation = new ProfilClientSerialisation();
+                serialisation = new ProfilClientSerialisation();
             }
             break;
-            
+
             /*case "consulter-clients": {
                 action = new ListerClientsAction();
                 serialisation = new ListeClientsSerialisation();
             }
             break; */
         }
-        if (action != null /*&& serialisation != null*/) {
+        if (action != null && serialisation != null) {
             action.executer(request); // Executer l'Action
-            //serialisation.serialiser(request, response); // Serialiser le resultat de l'Action
+            serialisation.serialiser(request, response); // Serialiser le resultat de l'Action
         } else { //Erreur : pas d'Action ou de Serialisation pour traiter cette requete
             response.sendError(400, "Bad request (pas d'Action ou de Serialisation pour "
                     + "traiter cette requete)");
--- src/main/java/com/mycompany/mavenwebproject/ihm/web/serialisation/ProfilClientSerialisation.java
+++ src/main/java/com/mycompany/mavenwebproject/ihm/web/serialisation/ProfilClientSerialisation.java
@@ -5,8 +5,14 @@
  */
 package com.mycompany.mavenwebproject.ihm.web.serialisation;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.mycompany.tdmaven.metier.modele.Client;
+import com.mycompany.tdmaven.metier.service.Service;
 import java.io.IOException;
-import javax.json.JsonObject;
+import java.io.PrintWriter;
+//import javax.json.JsonObject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -19,8 +25,17 @@ public class ProfilClientSerialisation extends Serialisation {
     @Override
     public void serialiser(HttpServletRequest request, HttpServletResponse response) 
             throws IOException {
-        JsonObject container = new JsonObject(); //Objet "conteneur Json" pour structurer les donnees a serialiser
+        JsonObject jsonClient = new JsonObject(); //Creation un objet Json pour un client
         
+        String mail = request.getParameter("mail");
+        String mdp = request.getParameter("mdp");
+        Service service = new Service();
+        Client client = service.authentifierClient(mail, mdp);
+        jsonClient.addProperty("id", client.getId());
         
+        PrintWriter out = this.getWriter(response);
+        Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+        gson.toJson(jsonClient, out);
+        out.close();
     } 
 }
